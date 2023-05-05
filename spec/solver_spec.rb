63f4697de8f5057fabc960b73a275efb718d737a
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-05 15:05:54 +0300

    add 0 test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,6 +15,9 @@ describe Solver do
     it 'shoul retur 120 when a num of 5 is passed as an argument' do
       expect(@solver.factorial(5)).to be(120)
     end
+    it 'shoul retur 1 when a num of 0 is passed as an argument' do
+      expect(@solver.factorial(0)).to be(1)
+    end
     it 'shoul raise error when a num of -1 is passed as an argument' do
       expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
     end
