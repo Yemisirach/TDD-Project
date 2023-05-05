4d8c23ae85a0bf7f114d9fa84397cbe7043d56a0
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-05 12:24:40 +0300

    fix merge

--- solver.rb
+++ solver.rb
@@ -1,3 +1,11 @@
 class Solver
-  
+ def factorial(n)
+    if n < 0
+    raise ArgumentError, "factorial is not defined for negative numbers"
+  elsif n == 0
+    return 1
+  else
+    return n * factorial(n-1)
+  end
+end
 end
