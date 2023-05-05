166a9d875596af0744248a9caac7cddf7695a3dd
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-05 11:13:16 +0300

    add instance test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,14 @@
 require_relative './solver'
 
 describe Book do
+  before(:each) do
+      @solver = Solver.new
+  end
 
+  describe 'book' do
+    it 'checks if its an instance of Solver' do
+      expect(@solver).to be_an_instance_of(Solver)
+    end
+  end
 
 end
