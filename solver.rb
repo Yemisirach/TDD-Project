e3200365c892b25dd3831ed6c4557995facc1258
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-05 13:03:53 +0300

    add fizz buzz methd

--- solver.rb
+++ solver.rb
@@ -1,17 +1,27 @@
 class Solver
- def factorial(n)
+  def factorial(n)
     if n < 0
-    raise ArgumentError, "factorial is not defined for negative numbers"
-  elsif n == 0
-    return 1
-  else
-    return n * factorial(n-1)
+      raise ArgumentError, 'factorial is not defined for negative numbers'
+    elsif n == 0
+      1
+    else
+      n * factorial(n - 1)
+    end
   end
-end
 
-def reverse_string(str)
-  str.reverse
-end
+  def reverse_string(str)
+    str.reverse
+  end
 
-
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
